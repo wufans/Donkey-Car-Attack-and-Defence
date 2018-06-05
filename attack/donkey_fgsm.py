d25147a1aa326ab7d3eea2f4ea8ba85f7e315aaf
FanWu <1140989765@qq.com>
2018-06-05 20:41:55 +0800

    add FGSM attack code

--- attack/donkey_fgsm.py
+++ attack/donkey_fgsm.py
@@ -4,16 +4,67 @@ import keras
 from donkeycar.parts.keras import KerasCategorical
 
 
-def fgm(model, x_image):
+# def fgm(model, x_image):
+#     """
+#     Fast gradient method to generate adversarial examples of donkeycar
+
+#     """
+#     x_adv_image = tf.identity(x_image) #返回相同tensor的op
+
+
+#    return x_adv_image
+def fgm(model, x, eps=0.01, epochs=1, sign=True, clip_min=0., clip_max=1.):
     """
-    Fast gradient method to generate adversarial examples of donkeycar
+    Fast gradient method.
 
+    :param model: A wrapper that returns the output as well as logits.
+    :param x: The input placeholder.
+    :param eps: The scale factor for noise.
+    :param epochs: The maximum epoch to run.
+    :param sign: Use gradient sign if True, otherwise use gradient value.
+    :param clip_min: The minimum value in output.
+    :param clip_max: The maximum value in output.
+
+    :return: A tensor, contains adversarial samples for each input.
     """
-    x_adv_image = tf.identity(x_image) #返回相同tensor的op
+    xadv = tf.identity(x) #返回相同tensor的op
 
+    ybar = model(xadv)
+    yshape = ybar.get_shape().as_list()
+    ydim = yshape[1]
 
-    return x_adv_image
+    indices = tf.argmax(ybar, axis=1)
+    target = tf.cond(
+        tf.equal(ydim, 1),
+        lambda: tf.nn.relu(tf.sign(ybar - 0.5)),
+        lambda: tf.one_hot(indices, ydim, on_value=1.0, off_value=0.0))
 
+    if 1 == ydim:
+        loss_fn = tf.nn.sigmoid_cross_entropy_with_logits
+    else:
+        loss_fn = tf.nn.softmax_cross_entropy_with_logits
+
+    if sign:
+        noise_fn = tf.sign
+    else:
+        noise_fn = tf.identity
+
+    eps = tf.abs(eps)
+
+    def _cond(xadv, i):
+        return tf.less(i, epochs)
+
+    def _body(xadv, i):
+        ybar, logits = model(xadv, logits=True)
+        loss = loss_fn(labels=target, logits=logits)
+        dy_dx, = tf.gradients(loss, xadv)
+        xadv = tf.stop_gradient(xadv + eps*noise_fn(dy_dx))
+        xadv = tf.clip_by_value(xadv, clip_min, clip_max)
+        return xadv, i+1
+
+    xadv, _ = tf.while_loop(_cond, _body, (xadv, 0), back_prop=False,
+                            name='fast_gradient')
+    return xadv
 
 
 def default_model():
@@ -83,7 +134,5 @@ if __name__ == '__main__':
 
     kl = KerasCategorical()
     model = kl.load(model_path)
-    x_adv = fgm(model,x_image)
+    x_adv = fgm(model,x_image)#生成FGSM对抗样本
 
-    print("Train a autopilot model")
-    train()
